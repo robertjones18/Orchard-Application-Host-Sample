356307908b280e4ed4191e467192fe96f919a358
Zoltán Lehóczky <lehoczky_zoltan@pyrocenter.hu>
2014-06-29 23:00:53 +0200

    Setup sample

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ using Orchard.Services;
 using Orchard.Settings;
 using Lombiq.OrchardAppHost.Services;
 using Orchard.Environment.Descriptor.Models;
+using Orchard.Setup.Services;
 
 namespace Lombiq.OrchardAppHost.Sample
 {
@@ -23,7 +24,9 @@ namespace Lombiq.OrchardAppHost.Sample
                 ModuleFolderPaths = new[] { @"E:\Projects\Munka\Lombiq\Orchard Dev Hg\src\Orchard.Web\Modules" },
                 CoreModuleFolderPaths = new[] { @"E:\Projects\Munka\Lombiq\Orchard Dev Hg\src\Orchard.Web\Core" },
                 ThemeFolderPaths = new[] { @"E:\Projects\Munka\Lombiq\Orchard Dev Hg\src\Orchard.Web\Themes" },
-                ImportedExtensions = new[] { new ShellExtensions { ShellName = ShellSettings.DefaultName, Extensions = new[] { typeof(Program).Assembly } } }
+                ImportedExtensions = new[] { typeof(Program).Assembly, typeof(IOrchardAppHost).Assembly },
+                DefaultShellFeatureStates = new[] { new DefaultShellFeatureState { ShellName = ShellSettings.DefaultName, EnabledFeatures = new[] { "Lombiq.OrchardAppHost", "Lombiq.OrchardAppHost.Sample.ShellEvents" } } },
+                DisableConfiguratonCaches = true
             };
 
             //var enabledFeatures = new[] { new ShellFeature { Name = "Settings" } };
@@ -44,20 +47,45 @@ namespace Lombiq.OrchardAppHost.Sample
 
                 Console.CancelKeyPress += (sender, e) => run = false;
 
+                // We can even run the setup on a new shell. A reference to Orchard.Setup is needed.
+                // The setup shouldn't run in a transaction.
+                //host.Run<ISetupService, ShellSettings>((setupService, shellSettings) =>
+                //{
+                //    Console.WriteLine("Running setup for the following shell: " + shellSettings.Name);
+                //    setupService.Setup(new SetupContext
+                //        {
+                //            SiteName = "Test",
+                //            AdminUsername = "admin",
+                //            AdminPassword = "password",
+                //            DatabaseProvider = "SqlCe",
+                //            Recipe = "Default"
+                //        });
+
+                //    Console.WriteLine("Setup done");
+                //}, ShellSettings.DefaultName, false);
+
+                //// After setup everything else should run in a separate scope.
+                //host.Run<ISiteService, ShellSettings>((siteService, shellSettings) =>
+                //{
+                //    Console.WriteLine(siteService.GetSiteSettings().SiteName);
+                //    Console.WriteLine(shellSettings.Name);
+                //});
+
                 while (run)
                 {
                     Console.WriteLine("Cycle");
-
-                    host.RunInTransaction(scope =>
-                    {
-                        Console.WriteLine(scope.Resolve<ISiteService>().GetSiteSettings().SiteName);
-                        Console.WriteLine(scope.Resolve < ShellSettings>().Name);
-                    });
 
                     host.Run<ILoggerService, IClock>((logger, clock) =>
                     {
                         logger.Error("Test log entry.");
                         Console.WriteLine(clock.UtcNow.ToString());
+                    });
+
+                    // Another overload of Run() for simple transaction handling and for using the work context scope directly.
+                    host.RunInTransaction(scope =>
+                    {
+                        Console.WriteLine(scope.Resolve<ISiteService>().GetSiteSettings().SiteName);
+                        Console.WriteLine(scope.Resolve<ShellSettings>().Name);
                     });
 
                     Console.WriteLine();
---
+++ Readme.md
@@ -0,0 +1,6 @@
+# Orchard Application Host Sample Readme
+
+
+
+- Note the configurations in App.config.
+- Note the project references (some of them explained in the AppHost's documentation).
