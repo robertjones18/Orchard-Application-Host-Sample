ed969c5320917874cb5a4aeb4bd901f028022cd1
Zoltán Lehóczky <lehoczky_zoltan@pyrocenter.hu>
2014-07-29 11:42:57 +0200

    Usings clean-up

--- Services/AsyncBackgroundTaskSample.cs
+++ Services/AsyncBackgroundTaskSample.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Lombiq.OrchardAppHost.Environment.Tasks;
 
 namespace Lombiq.OrchardAppHost.Sample.Services
